a4aa49c2fbff18ec92f4cabf1a976a65fec60150
Frank Yang <puilp0502@gmail.com>
2019-11-25 04:23:17 +0900

    Add express-session middleware

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const createError = require('http-errors');
 const logger = require('morgan');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 const router = require('./router');
+const dbInfo = require('./database/db.info.js');
 
 const path = require('path');
 
 const app = express();
+const sessionStore = new MySQLStore(dbInfo.local);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -13,6 +17,15 @@ app.set('view engine', 'ejs');
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use(logger('dev'));
+// Create and use express-session middlewared
+app.use(session(
+	{
+		secret: process.env.SESSION_SECRET || "super duper secret key",
+		store: sessionStore,
+		resave: false,
+		saveUninitialized: false,
+	}
+));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router);
